f7ab26bd0b119c36c3266fdbe038e6946a090557
Jonathan Morris <jonathan_wellington@icloud.com>
2024-01-14 14:15:41 -0800

    Setup

---
+++ Renter-Helper/Renter-Helper/Renter_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Renter_HelperApp.swift
+//  Renter-Helper
+//
+//  Created by Jonathan Morris on 1/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Renter_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
