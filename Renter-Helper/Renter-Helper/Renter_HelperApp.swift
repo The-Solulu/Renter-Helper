5786b8b34c7980a78583a51ce7b529c12f55ae61
Jonathan Morris <jonathan_wellington@icloud.com>
2024-01-19 13:15:00 -0800

    Changes

--- Renter-Helper/Renter-Helper/Renter_HelperApp.swift
+++ Renter-Helper/Renter-Helper/Renter_HelperApp.swift
@@ -6,6 +6,14 @@
 //
 
 import SwiftUI
+import FirebaseCore
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct Renter_HelperApp: App {
