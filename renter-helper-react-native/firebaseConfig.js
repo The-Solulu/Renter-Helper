bfc1e6dd894b607ae503c6be791f69f9d4e7c43f
Jonathan Morris <jonathan_wellington@icloud.com>
2024-01-20 17:46:50 -0800

    Update

---
+++ renter-helper-react-native/firebaseConfig.js
@@ -0,0 +1,24 @@
+import { initializeApp } from 'firebase/app';
+
+// Optionally import the services that you want to use
+// import {...} from "firebase/auth";
+// import {...} from "firebase/database";
+// import {...} from "firebase/firestore";
+// import {...} from "firebase/functions";
+// import {...} from "firebase/storage";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCAh3Rz9Z98dDWO8VZA0OegaQriGKgf7NE",
+  authDomain: "renter-helper-d752c.firebaseapp.com",
+  projectId: "renter-helper-d752c",
+  storageBucket: "renter-helper-d752c.appspot.com",
+  messagingSenderId: "733942461222",
+  appId: "1:733942461222:web:e0ece75df01398eee987e7",
+  measurementId: "G-KE29DR5HLB"
+};
+
+const app = initializeApp(firebaseConfig);
+
+// For more information on how to access Firebase in your project,
+// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
