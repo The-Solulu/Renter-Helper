5d253066f4711ddc00aaf9fbfac043d688233263
Jonathan Morris <jonathan_wellington@icloud.com>
2024-01-20 16:44:48 -0800

    Changes

--- renter-helper-react-native/Views/Home.js
+++ renter-helper-react-native/Views/Home.js
@@ -1,17 +1,17 @@
-import { useState } from 'react';
-import { TextInput, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 
-function Home() {
-    const [text, setText] = useState('');
+function homeCard() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <TextInput
-                style={{ height: 40 }}
-                placeholder="Type here to translate!"
-                onChangeText={newText => setText(newText)}
-                defaultValue={text}
-            />
+            <Image source={{ uri: 'https://reactjs.org/logo-og.png' }}
+                style={{ width: 200, height: 200 }} />
         </View>
+    );
+}
+
+function Home() {
+    return (
+        homeCard()
     );
 }
 
