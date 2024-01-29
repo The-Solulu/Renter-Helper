1c005fc1c651727d4ca97c0775df2486f71383b1
Parupupapi <156978613+Parupupapi@users.noreply.github.com>
2024-01-29 15:09:08 -0800

    Line 16 on app.js change it to true or false if u want to work on the login page or the other stuff

--- renter-helper-react-native/App.js
+++ renter-helper-react-native/App.js
@@ -4,62 +4,59 @@ import { Messages } from './Views/Messages';
 import { Settings } from './Views/Settings';
 import 'react-native-gesture-handler';
 
-import { Ionicons, Feather } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import getCollections from './firebase.js';
 import Chat from './Views/Chat';
+import AreYouARenterOrOwner from './Views/AreYouARenterOrOwner.js';
 
-// const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
-
-
-/*const MessageStack = ({navigation}) => (
-  <Stack.Navigator>
-    <Stack.Screen name="Messages" component={Messages} />
-    <Stack.Screen name="Chat" component={Chat} />
-  </Stack.Navigator>
-)*/
+var Signed_in = false;
 
 export default function App() {
-  getCollections();
-  return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="home" color={color} size={size} />
-            ),
-          }}
-        />
-        <Tab.Screen name="People" component={People}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="person" size={size} color={color} />
-            ),
-          }}
-        />
-        <Tab.Screen name="Messages" component={Messages}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="chatbubble-outline" size={size} color={color} />
-            ),
-          }}
-
-        />
-
-        <Tab.Screen name="Settings" component={Settings}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="settings" size={size} color={color} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+  getCollections(); 
+  if (Signed_in) {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={Home}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="home" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen name="People" component={People}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="person" size={size} color={color} />
+              ),
+            }}
+          />
+          <Tab.Screen name="Messages" component={Messages}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="chatbubble-outline" size={size} color={color} />
+              ),
+            }}
+          />
+          <Tab.Screen name="Settings" component={Settings}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="settings" size={size} color={color} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  } else {
+    return (
+      <AreYouARenterOrOwner />
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -69,5 +66,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-
 });
